d1706c0eb20eed63741ed496421a57d96702585d
ArcadiusM <arcadius.modzelewski@gmail.com>
2020-03-23 00:16:36 +0100

    Add cost function for reularized log regression

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -8,8 +8,8 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+%J = 0;
+%grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -17,8 +17,13 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+J = (1 / m) * ((-y' * log(sigmoid(X*theta))) - (1-y)' * log(1 - sigmoid(X*theta))) + (lambda / (2 * m)) * (theta(2:length(theta)))' * theta(2:length(theta));
 
 
+thetaZero = theta;
+thetaZero(1) = 0;
+
+grad = (1 / m) * (sigmoid(X*theta)-y)' * X + lambda / m * thetaZero';
 
 
 
