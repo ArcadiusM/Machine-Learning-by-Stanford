49e5c8cd929be796b9680fe10784ca49d906c846
ArcadiusM <arcadius.modzelewski@gmail.com>
2020-03-31 14:11:34 +0200

    Add regularized and vectorized log cost function

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -38,11 +38,13 @@ grad = zeros(size(theta));
 
 
 
+J = (1 / m) * ((-y' * log(sigmoid(X*theta))) - (1-y)' * log(1 - sigmoid(X*theta))) + (lambda / (2 * m)) * (theta(2:length(theta)))' * theta(2:length(theta));
 
 
+thetaZero = theta;
+thetaZero(1) = 0;
 
-
-
+grad = (1 / m) * (sigmoid(X*theta)-y)' * X + lambda / m * thetaZero';
 
 
 % =============================================================
